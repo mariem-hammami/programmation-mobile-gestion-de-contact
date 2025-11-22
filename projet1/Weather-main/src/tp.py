e01b17eaa45a250819826138d5c4f668cf0626ad
Mariem <mariem@example.com>
2025-11-22 15:02:21 +0100

    derniere modifivation

--- projet1/Weather-main/src/tp.py
+++
@@ -1,5 +0,0 @@
-grade=[70,90,80,65,70]
-subject=['Math','Marketing','Production','Programming','Accounting']
-import matplotlib.pyplot as plt
-plt.plot(subject,grade)
-plt.title('Osama Hassan - Student Result')
